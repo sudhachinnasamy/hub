c355102ee1a8f02c1f78d52f9754809d666acd7f
sudhachinnasamy <30309481+sudhachinnasamy@users.noreply.github.com>
2017-07-20 11:55:10 +0530

    pos.java

---
+++ pos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Pos{  
+     public static void main(String args[])
+     {  
+       int number;
+       Scanner sc=new Scanner(System.in);  
+     
+       System.out.println("Enter your number");  
+       int number=sc.nextInt();  
+         
+      }    
+      if(number>0)    
+       System.out.println("positive ");    
+      else    
+       System.out.println(" negative");    
+    }  
+    
